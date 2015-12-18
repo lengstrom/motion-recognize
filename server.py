1f83652db8e7413c4f6bf927331163faa835a9da
lengstrom <lengstrom@users.noreply.github.com>
2015-12-18 02:13:31 -0500

    made static dir

--- server.py
+++ server.py
@@ -8,10 +8,16 @@ class UploadHandler(tornado.web.RequestHandler):
     def post(self):
         self.write("success!")
 
+class StaticHandler(tornado.web.StaticFileHandler):
+    def parse_url_path(self, path):
+        if not path or path.endswith('/'):
+            path = path + 'index.html'
+        return path
+
 if __name__ == "__main__":
     app = tornado.web.Application([
         (r"/upload", UploadHandler),
-        (r"/(.*)", tornado.web.StaticFileHandler, {'path':os.getcwd()})
+        (r"/(.*)", tornado.web.StaticFileHandler, {'path':os.path.join(os.getcwd(), 'static')})
     ])
     server = tornado.httpserver.HTTPServer(app)
     server.listen(5000)
